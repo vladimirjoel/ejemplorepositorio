cd5314fc9d7eb3069b3927fd8cef5adb0d5f0615
Pol <Pol@Pol-HP>
2015-08-11 18:35:38 -0500

    nueva funcion

--- Ejemplo/src/com/uigv/ejemplo/MainActivity.java
+++ Ejemplo/src/com/uigv/ejemplo/MainActivity.java
@@ -17,6 +17,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		
+	}
+	public void ingresaar(){
+		
+		
 	}
 
 }
